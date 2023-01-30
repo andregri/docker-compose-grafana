35d321e8199d7e0c3b2df98a1fdde4d9b7c79db7
Andrea Grillo <andrea.grillo96@live.com>
2023-01-30 14:12:39 +0100

    add mongodb to app stack

--- app/nginx-flask-mysql/backend/hello.py
+++ app/nginx-flask-mysql/backend/hello.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 import mysql.connector
 from pymongo import MongoClient
+import requests
 
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from prometheus_client import make_wsgi_app
@@ -66,6 +67,26 @@ def todo():
     except:
         return "Server not available"
     return "Hello from the MongoDB client!\n"
+
+
+@app.route('/insert_documents')
+def insert_documents():
+    # Next we define the database we are using.
+    # It does not have to exist first, like with relational databases.
+    db = client.get_database('coin_markets')
+
+    # Now, we make the API call and prices the results to the terminal.
+    prices = requests.get('https://api.coingecko.com/api/v3//coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    prices = prices.json()
+    print(prices)
+
+    # We define the collection we will store this data in,
+    # which is created dynamically like the database,
+    # and insert the data into the collection.
+    db_prices = db.get_collection('prices')
+    inserted = db_prices.insert_many(prices)
+    # Print a count of documents inserted.
+    return "Documents inserted: " + str(len(inserted.inserted_ids)) + " documents inserted"
 
 
 if __name__ == '__main__':
